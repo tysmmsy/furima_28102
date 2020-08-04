1ce223de84c624481517822ecad811953a32b095
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-04 16:25:19 +0900

    不要カラムがあった為、データベースのリセット

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,13 @@ ActiveRecord::Schema.define(version: 2020_08_03_225721) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birthday", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
