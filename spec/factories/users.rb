3be22e8ccd5d0ac063fe790f44c2f63967531521
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-04 18:22:55 +0900

    userの値を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do
     Faker::Config.locale = :ja
     nickname              { Faker::Lorem.sentence }
-    family_name           { '山田' }
+    last_name             { '山田' }
     first_name            { '太郎' }
-    family_name_kana      { 'ヤマダ' }
+    last_name_kana        { 'ヤマダ' }
     first_name_kana       { 'タロウ' }
     birthday              { '1991-04-10' }
     email                 { Faker::Internet.free_email }
