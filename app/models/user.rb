9899933dc5c0fa4ee68b807e99ef53ba362d4814
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-04 14:34:06 +0900

    userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
+  validates :confirm_password, confirmation: true
+  validates :birthday
+  validates :last_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ } 
 end
