9863036031a18112eab29f1b474c2104e80a399e
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-05 12:10:16 +0900

    ActiveHash カテゴリーの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :category_id, numericality: { other_than: 1 } 
 end
