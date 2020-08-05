383127f72a642fb5e590fd5d2c058459bb82870b
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-05 12:44:52 +0900

    itemモデル バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :item_condition, :delivery_fee, :shipping_regions, :shipping_days
 
-  validates :category_id, :item_condition_id, :delivery_fee_id, :shipping_regions_id, :shipping_days_id numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :user_id, :name, :description, :price
+    validates :category_id, :item_condition_id, :delivery_fee_id, :shipping_regions_id, :shipping_days_id numericality: { other_than: 1 } 
+    validates :price_range_valid?
+  end
+
+  def price_range_valid?
+    if price < 300 || price > 9999999
+      errors.add(:price, "価格設定を変えてください")
+    end
+  end
 end
