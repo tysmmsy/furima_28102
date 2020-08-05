aff2a73f6cd4ef9d5a5961eed29b6141533553bf
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-05 12:19:22 +0900

    ActiveHash 配送料の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :item_condition 
+  belongs_to_active_hash :category, :item_condition, :delivery_fee
 
-  validates :category_id, :item_condition_id, numericality: { other_than: 0 } 
+  validates :category_id, :item_condition_id, :delivery_fee_id numericality: { other_than: 1 } 
 end
