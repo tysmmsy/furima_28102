c89aa6acd9461fbbf207b7d85a02d0548dff79f2
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-05 11:36:04 +0900

    itemに関するルーティングの設定、ログインしているユーザーだけが、出品ページへ遷移できることの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def show
+  end
+
+  def new
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def create
+  end
+  
 end
