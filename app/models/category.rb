ed7fa9f626840a8f242a1030c3e1f89a00d31b77
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-05 12:33:02 +0900

    ActiveHash 発送日程の設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,13 +2,14 @@ class Category < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
     { id: 1, name: 'レディース' },
-    { id: 2, name: 'ベビー・キッズ' },
-    { id: 3, name: 'インテリア・住まい・小物' },
+    { id: 2, name: 'メンズ' },
+    { id: 3, name: 'ベビー・キッズ' },
+    { id: 4, name: 'インテリア・住まい・小物' },
     { id: 4, name: '本・音楽・ゲーム' },
-    { id: 5, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 6, name: '家電・スマホ・カメラ' },
-    { id: 7, name: 'スポーツ・レジャー' },
-    { id: 8, name: 'ハンドメイド' },
-    { id: 9, name: 'その他' }
+    { id: 6, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 7, name: '家電・スマホ・カメラ' },
+    { id: 8, name: 'スポーツ・レジャー' },
+    { id: 9, name: 'ハンドメイド' },
+    { id: 10, name: 'その他' }
   ]
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :item_condition, :delivery_fee, :shipping_regions
+  belongs_to_active_hash :category, :item_condition, :delivery_fee, :shipping_regions, :shipping_days
 
-  validates :category_id, :item_condition_id, :delivery_fee_id, :shipping_regions_id numericality: { other_than: 1 } 
+  validates :category_id, :item_condition_id, :delivery_fee_id, :shipping_regions_id, :shipping_days_id numericality: { other_than: 1 } 
 end
