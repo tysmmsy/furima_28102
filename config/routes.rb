461b21edce74d04cafd4233b3ac17426a90a1c09
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-07 15:16:39 +0900

    Revert "Revert "出品者だけが編集ページに遷移できるように設定""
    This reverts commit 15c70be8e7747fb1e8f3f93372a0a6a35509a536.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root "items#index"
   devise_for :users
-  resources :items, only: [:index, :show, :new, :create]
+  resources :items, only: [:index, :show, :new, :create, :edit]
 end
