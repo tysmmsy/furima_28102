44a5bda8ae4b91b594a1cee93de0ea26c3f32a81
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-07 23:17:56 +0900

    addressesモデル作成、並びにorders、addressesテーブル作成

--- db/migrate/20200803225721_create_items.rb
+++ db/migrate/20200803225721_create_items.rb
@@ -8,8 +8,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id,          null: false
       t.integer :item_condition_id,    null: false
       t.integer :delivery_fee_id,      null: false
-      t.integer :shipping_region_id,  null: false
-      t.integer :shipping_day_id,     null: false
+      t.integer :shipping_region_id,   null: false
+      t.integer :shipping_day_id,      null: false
       t.timestamps
     end
   end
