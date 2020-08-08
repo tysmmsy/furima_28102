4a144cec6a725a766266d55646ce98712f158e0d
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-08 13:10:50 +0900

    購入ページに遷移するように設定(ログインしていないユーザーが遷移するとログインページに遷移)、またpay.jsを読み込めるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :check_item, only: :index
+  before_action :move_to_index, only: :index
+
   def index
+    
   end
 
   def new
@@ -9,8 +13,11 @@ class OrdersController < ApplicationController
   def create
     @order = OrderAddress.new(order_params)
     @order.save
+  end
 
-    private
+  private
+
+  def order_address
     params.require(:order_address).permit(:item,
                                           :postal_code,
                                           :shipping_region,
@@ -19,6 +26,14 @@ class OrdersController < ApplicationController
                                           :building_name,
                                           :phone_number,
                                          )
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def check_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root "items#index"
   devise_for :users
-  resources :items
-  resources :orders, only: [:index, :create]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
