a9a41648bfc3a8ff16fe8c353826fb5097d04e19
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-10 20:48:02 +0900

    test

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -16,6 +16,7 @@ const pay = () => {
 
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
+        console.log(response)
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
